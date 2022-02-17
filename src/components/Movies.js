18cfda7d8dbc76b904a5d6286c4ea3330b0b307c
adwait kulkarni <adwaitkulkarni2211@gmail.com>
2022-02-17 09:47:16 +0530

    movie title and poster

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,123 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "../banner.jpg";
+import axios from "axios";
+import { TailSpin } from "react-loader-spinner";
 
 function Movies() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/trending/movie/week?api_key=97310bb852aa576566d673aa9cfd45bf"
+      )
+      .then((response) => {
+        console.log(response);
+        setMovies(response.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
   return (
     <>
       <div className="mb-8">
         <div className="mt-8 mb-8 font-bold text-2xl text-center">
           Trending Movies
         </div>
-        <div className="flex flex-wrap justify-center">
-          <div
-            className={`
-            bg-[url(${Image})]
-            h-[25vh] w-[150px]
-            md:h-[30vh] md:w-[250px]
-            bg-center bg-cover
-            rounded-xl
-            flex items-end
-            m-4
-            hover:scale-110
-            ease-out duration-300
-          `}
-          >
-            <div className="p-1 text-xl font-bold text-slate-200 bg-slate-900 w-full rounded-b-xl items-end flex justify-center">
-              Title
-            </div>
+        {movies.length == 0 ? (
+          <div className="flex justify-center">
+            <TailSpin
+              height="100"
+              width="100"
+              color="grey"
+              ariaLabel="loading"
+            />
           </div>
-
-          <div
-            className={`
-            bg-[url(${Image})]
-            h-[25vh] w-[150px]
-            md:h-[30vh] md:w-[250px]
-            bg-center bg-cover
-            rounded-xl
-            flex items-end
-            m-4
-            hover:scale-110
-            ease-out duration-300
-          `}
-          >
-            <div className="p-1 text-xl font-bold text-slate-200 bg-slate-900 w-full rounded-b-xl items-end flex justify-center">
-              Title
-            </div>
+        ) : (
+          <div className="flex flex-wrap justify-center">
+            {movies.map((movie) => (
+              <div
+                className={`
+                  bg-[url(https://image.tmdb.org/t/p/w500${movie.backdrop_path})]
+                  h-[25vh] w-[150px]
+                  md:h-[30vh] md:w-[250px]
+                  bg-center bg-cover
+                  rounded-xl
+                  flex items-end
+                  m-4
+                  hover:scale-110
+                  ease-out duration-300
+                `}
+              >
+                <div className="p-1 text-xl font-bold text-slate-200 bg-slate-900 w-full rounded-b-xl items-end flex justify-center">
+                  {movie.title}
+                </div>
+              </div>
+            ))}
           </div>
-
-          <div
-            className={`
-            bg-[url(${Image})]
-            h-[25vh] w-[150px]
-            md:h-[30vh] md:w-[250px]
-            bg-center bg-cover
-            rounded-xl
-            flex items-end
-            m-4
-            hover:scale-110
-            ease-out duration-300
-          `}
-          >
-            <div className="p-1 text-xl font-bold text-slate-200 bg-slate-900 w-full rounded-b-xl items-end flex justify-center">
-              Title
-            </div>
-          </div>
-
-          <div
-            className={`
-            bg-[url(${Image})]
-            h-[25vh] w-[150px]
-            md:h-[30vh] md:w-[250px]
-            bg-center bg-cover
-            rounded-xl
-            flex items-end
-            m-4
-            hover:scale-110
-            ease-out duration-300
-          `}
-          >
-            <div className="p-1 text-xl font-bold text-slate-200 bg-slate-900 w-full rounded-b-xl items-end flex justify-center">
-              Title
-            </div>
-          </div>
-
-          <div
-            className={`
-            bg-[url(${Image})]
-            h-[25vh] w-[150px]
-            md:h-[30vh] md:w-[250px]
-            bg-center bg-cover
-            rounded-xl
-            flex items-end
-            m-4
-            hover:scale-110
-            ease-out duration-300
-          `}
-          >
-            <div className="p-1 text-xl font-bold text-slate-200 bg-slate-900 w-full rounded-b-xl items-end flex justify-center">
-              Title
-            </div>
-          </div>
-
-          <div
-            className={`
-            bg-[url(${Image})]
-            h-[25vh] w-[150px]
-            md:h-[30vh] md:w-[250px]
-            bg-center bg-cover
-            rounded-xl
-            flex items-end
-            m-4
-            hover:scale-110
-            ease-out duration-300
-          `}
-          >
-            <div className="p-1 text-xl font-bold text-slate-200 bg-slate-900 w-full rounded-b-xl items-end flex justify-center">
-              Title
-            </div>
-          </div>
-          
-        </div>
+        )}
       </div>
     </>
   );
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function Pagination() {
+  const [pageNumber, setPage] = useState(1);
+
+  const nextPage = () => {
+    setPage(pageNumber + 1);
+  }
+
+  const prevPage = () => {
+    if(pageNumber > 1) {
+      setPage(pageNumber - 1);
+    }
+  }
+
   return (
-    <div className='mb-8 w-full flex justify-center'>
-        <button className='p-2 border-2 border-r-0 border-indigo-500 text-indigo-500 rounded-l-xl'>Previous</button>
-        <button className='p-2 border-2 border-r-0 border-indigo-500 text-indigo-500'>2</button>
-        <button className='p-2 border-2 border-indigo-500 text-indigo-500 rounded-r-xl'>Next</button>
+    <div className="mb-8 w-full flex justify-center">
+      <button className="p-2 border-2 border-r-0 border-indigo-500 text-indigo-500 rounded-l-xl" onClick={prevPage}>
+        Previous
+      </button>
+      <button className="p-2 border-2 border-r-0 border-indigo-500 text-indigo-500">
+        {pageNumber}
+      </button>
+      <button className="p-2 border-2 border-indigo-500 text-indigo-500 rounded-r-xl" onClick={nextPage}>
+        Next
+      </button>
     </div>
-  )
+  );
 }
 
-export default Pagination
+export default Pagination;
